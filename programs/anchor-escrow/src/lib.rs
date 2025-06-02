7a0713f43282bea464206c4eee0207619a8aba6d
ZAC <zachery.johnson.dev@gmail.com>
2025-06-03 06:03:57 +0900

    Deployment on Devnet

---
+++ programs/anchor-escrow/src/lib.rs
@@ -0,0 +1,30 @@
+use anchor_lang::prelude::*;
+mod contexts;
+use contexts::*;
+mod states;
+
+declare_id!("EZUZxExAfPrc4oQt5sXJcyNg4w113erzjkaPEX87BHEw");
+#[program]
+pub mod anchor_escrow {
+    use super::*;
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        initializer_amount: u64,
+        taker_amount: u64,
+    ) -> Result<()> {
+        ctx.accounts
+            .initialize_escrow(seed, &ctx.bumps, initializer_amount, taker_amount)?;
+        ctx.accounts.deposit(initializer_amount)
+    }
+
+    pub fn cancel(ctx: Context<Cancel>) -> Result<()> {
+        ctx.accounts.refund_and_close_vault()
+    }
+
+    pub fn exchange(ctx: Context<Exchange>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()
+    }
+}
